e8edfd1905ec418df09adf5f7591430c47f3abd1
Terrance G <goldsberryt@gmail.com>
2020-11-08 16:41:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Vacation Budget Planner!");
+
+            Console.WriteLine("Please enter your name: ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Where would you like to go " + name + "? " + "Mexico or Jamaica?");
+
+            string country = Console.ReadLine();
+
+            Console.WriteLine("How many days would you like to spend in " + country + "?");
+
+            int days = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How much money would you like to spend in " + country + "?");
+
+            int money = Convert.ToInt16(Console.ReadLine());
+
+            int perDay = money / days;
+
+            Console.WriteLine("Hey " + name + ", you are going to " + country + " for " + days + " with $" + money
+
+              +  " for spending money. You can spend $" + perDay +" per day.");
+
+            Console.ReadLine();
+        }
+    }
+}
